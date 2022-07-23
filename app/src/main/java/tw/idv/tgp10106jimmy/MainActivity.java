b66309c4efbb33b54b0a8cfa8ae5ed27ab6441bb
JIMMY\User <roc110183@gmail.com>
2022-07-23 16:22:47 +0800

    Android Project Initialize

---
+++ app/src/main/java/tw/idv/tgp10106jimmy/MainActivity.java
@@ -0,0 +1,14 @@
+package tw.idv.tgp10106jimmy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
